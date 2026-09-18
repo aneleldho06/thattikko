eb883afa8d6518a7d95f28a15274d7cbca18f154
Anel Eldho <iamaneleldho@gmail.com>
2026-09-18 13:37:37 +0000

    added google tag in the root

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -111,6 +111,15 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z5V6Y3LLJ2"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Z5V6Y3LLJ2');
+          </script>
       </head>
       <body>
         {children}
